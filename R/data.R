f5acff5988d2470f331ee98c285713f5bddfb8a6
Markus Lux <mlux@ml1.net>
2017-11-16 14:27:00 +0100

    remove sample data from package (is now in extra/data folder as rds)

--- R/data.R
+++ R/data.R
@@ -1,11 +1,3 @@
 #' Example DensityData object, containing three samples, 16 populations, and two channels each.
 #'
 "flSampleDensdat"
-
-#' Evaluation data to gate B-cells, used in flowLearnVignette.
-#'
-"flSampleBcellEvaluationData"
-
-#' Example data to show how to construct DensityData objects from flowFrames, used in flowLearnVignette.
-#'
-"flSampleFlowFrame"
